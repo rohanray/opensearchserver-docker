47e8008a61575089f02ed73eee0d579d47625744
Rohan Ray <rohanray@gmail.com>
2019-04-25 19:14:39 +0530

    inter mac

--- opensearchserver/start_opensearchserver.sh
+++ opensearchserver/start_opensearchserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MEMORY=${MEMORY:-1512m}
+MEMORY=${MEMORY:-5000m}
 # always stops service
 service opensearchserver stop
-sed -e 's/#JAVA_OPTS="-Xms1G -Xmx1G"/JAVA_OPTS="-Xms'$MEMORY' -Xmx'$MEMORY'"/' -i /etc/opensearchserver
+sed -e 's/#JAVA_OPTS="-Xms4G -Xmx8G"/JAVA_OPTS="-Xms'$MEMORY' -Xmx'$MEMORY'"/' -i /etc/opensearchserver
 service opensearchserver start
